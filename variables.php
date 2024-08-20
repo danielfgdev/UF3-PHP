c5f09a80a9c7cbfa490c9a2d2507e264794d872f
danielfgdev <danielfgdev@gmail.com>
2024-08-20 17:59:40 +0200

    Initial commit

---
+++ variables.php
@@ -0,0 +1,19 @@
+<?php
+$cadena = "Esta es una cadena de prueba"; // Declarando una cadena
+$booleano = true; // Declarando un booleano
+$float = 7.55; // Declarando un punto flotante
+echo "La cadena es " . $cadena . "<br/>";
+echo "El número de punto flotante es " . $float;
+?>
+
+
+<br><br>
+
+
+<?php
+// Modificar el valor de una variable en tiempo de ejecución
+$mi_variable = "Este es el primer valor de la variable";
+echo $mi_variable; // Lo imprimimos
+$mi_variable = "2"; //Lo modificamos
+echo "Ahora el valor es " . $mi_variable;
+?>
