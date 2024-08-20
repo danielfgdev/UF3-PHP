16638e8bde82ff062cf32d14d51e6c5b17b3559b
danielfgdev <danielfgdev@gmail.com>
2024-08-20 19:52:40 +0200

    changes

---
+++ comillas.php
@@ -0,0 +1,10 @@
+<?php
+$nombre = "Pedro Pérez";
+echo 'La variable \$nombre es $nombre';
+echo "<br>";
+echo "La variable \$nombre es $nombre";
+echo "<br>";
+echo 'La variable \$nombre es $nombre';
+echo "<br>";
+echo "El valor de la variable \$nombre es $nombre";
+?>
---
+++ constantes.php
@@ -0,0 +1,4 @@
+<?php
+define("MENSAJE", "Hola Mundo"); // Define la constante
+echo MENSAJE; // Imprime el mensaje
+?>
