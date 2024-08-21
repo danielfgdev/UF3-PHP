556d3a00d3ed86714fa7267861866c4efb2b7041
danielfgdev <danielfgdev@gmail.com>
2024-08-22 01:49:53 +0200

    changes

---
+++ operadores.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores</title>
+</head>
+
+<body>
+
+    <h1 style="color:blue">Operadores</h1>
+
+    <?php
+
+    // $sistema_operativo = "MacOS";
+    // echo "Sistema operativo: $sistema_operativo<br>";
+    // switch ($sistema_operativo) {
+    //     case 'Windows':
+    //         echo "Su sistema operativo lo vende la empresa Microsoft.<br/>";
+    //         break;
+    //     case 'Linux':
+    //         echo 'Su sistema operativo fue creado a partir del código de Linus Torvalds.';
+    //         break;
+    //     case 'UNIX':
+    //         echo 'Su sistema operativo es considerado el mejor de la historia.';
+    //         break;
+    //     default:
+    //         echo "Posiblemente su sistema operativo sea de Apple.<br>";
+    // }
+    
+    ?>
+
+    <?php
+    // $estado_civil = 'Comprometid@';
+    // echo "Tu estado civil es $estado_civil.<br/>";
+    
+    // switch ($estado_civil) {
+    //     case 'Casad@':
+    //         echo '¡Enhorabuena!<br>';
+    //         break;
+    //     case 'Viud@':
+    //         echo 'Lamentamos su pérdida.<br>';
+    //         break;
+    //     case 'Comprometid@':
+    //         echo '¡Le deseamos suerte en su matrimonio!<br>';
+    //         break;
+    //     case 'Solter@':
+    //         echo '¿Está buscando novi@?<br>';
+    //         break;
+    //     default:
+    //         echo "Entendemos que es Ud muy discret@.<br>";
+    //         break;
+    // }
+    ?>
+
+    <?php
+
+    // function edad($edad)
+    // {
+    //     echo "Tu edad es $edad.<br/>";
+    //     if ($edad >= 18) {
+    //         echo "Eres mayor de edad.<br/>";
+    //     } else {
+    //         echo "Eres menor de edad.<br/>";
+    //     }
+    // }
+    
+    // $edad = 14;
+    // edad($edad);
+    // edad(38);
+    // edad(18);
+    
+    ?>
+
+    <?php
+
+    // function saludo()
+    // {
+    //     echo "Hola.<br>";
+    // }
+    
+    // function edad($edad)
+    // {
+    //     echo "Tu edad es $edad.<br />";
+    //     if ($edad < 18) {
+    //         echo "No puedes comprar cerveza.<br />";
+    //     }
+    //     if ($edad >= 18 && $edad <= 21) {
+    //         echo "En algunos países puedes comprar cerveza y en otros no.<br />";
+    //     }
+    //     if ($edad > 21) {
+    //         echo "Puedes comprar cerveza, recuerda beber con moderación.<br />";
+    //     }
+    //     if ($edad > 90) {
+    //         echo "¿Su médico de cabecera le permite tomar cerveza?<br />";
+    //     }
+    // }
+    
+    // saludo();
+    // $edad = 15;
+    // edad($edad);
+    // $edad = 18;
+    // edad($edad);
+    // $edad = 45;
+    // edad($edad);
+    // $edad = 96;
+    // edad($edad);
+    // saludo();
+    // edad(15);
+    // edad(96);
+    
+    ?>
+</body>
+
+</html>
