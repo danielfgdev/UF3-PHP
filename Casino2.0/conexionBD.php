b215e25126c38c6af12103d38b1b06bd2d7565f6
danielfgdev <danielfgdev@gmail.com>
2024-10-16 19:52:01 +0200

    MAMP

--- Casino2.0/conexionBD.php
+++ Casino2.0/conexionBD.php
@@ -3,7 +3,7 @@
 Data Objects). Here's a breakdown of what each part does: */
 $cadena_conexion = 'mysql:dbname=casino;host=127.0.0.1:3306';
 $usuario = 'root';
-$clave = '';
+$clave = 'root';
 try {
     $pdo = new PDO($cadena_conexion, $usuario, $clave);
     // $db = new PDO($cadena_conexion, $usuario, $clave, array(PDO::ATTR_PERSISTENT => true));
